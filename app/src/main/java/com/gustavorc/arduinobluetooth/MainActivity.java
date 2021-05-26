c6752c2061bf6e5de742429fdda0ad17b52919d0
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-26 17:22:21 -0500

    metodo on click

--- app/src/main/java/com/gustavorc/arduinobluetooth/MainActivity.java
+++ app/src/main/java/com/gustavorc/arduinobluetooth/MainActivity.java
@@ -21,5 +21,17 @@ public class MainActivity extends AppCompatActivity {
         btnOff = findViewById(R.id.btnOff);
         tvtMensaje = findViewById(R.id.tvtMensaje);
         btnDesconectar = findViewById(R.id.btnDesconectar);
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+
+            }
+        });
+
+
     }
+
+
+
 }

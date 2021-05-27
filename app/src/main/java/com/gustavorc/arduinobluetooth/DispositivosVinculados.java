024a413186be4833c8b96c3b890498dde3ae04e3
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-26 19:14:07 -0500

    metodos para bluetooth

--- app/src/main/java/com/gustavorc/arduinobluetooth/DispositivosVinculados.java
+++ app/src/main/java/com/gustavorc/arduinobluetooth/DispositivosVinculados.java
@@ -18,7 +18,7 @@ public class DispositivosVinculados extends AppCompatActivity {
 
     // Declaracion de campos
     private BluetoothAdapter mBtAdapter;
-    private ArrayAdapter mPairedDevicesArrayAdapt
+    private ArrayAdapter mPairedDevicesArrayAdapt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

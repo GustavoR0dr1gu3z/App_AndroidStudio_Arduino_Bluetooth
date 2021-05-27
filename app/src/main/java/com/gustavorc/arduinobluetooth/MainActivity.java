da9f8ad278b054753af1e5072e75d2ec7e4547d3
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-26 20:32:23 -0500

    class Activity Metodos

--- app/src/main/java/com/gustavorc/arduinobluetooth/MainActivity.java
+++ app/src/main/java/com/gustavorc/arduinobluetooth/MainActivity.java
@@ -86,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
         btnOn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-
+                MyConexionBT.write("i");
             }
         });
 

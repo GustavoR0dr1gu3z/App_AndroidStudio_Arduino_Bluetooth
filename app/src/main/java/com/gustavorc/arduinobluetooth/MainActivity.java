e46fc92fe184c41a349b54e92cfb35bba94766d1
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-26 19:41:39 -0500

    class Activity Metodos

--- app/src/main/java/com/gustavorc/arduinobluetooth/MainActivity.java
+++ app/src/main/java/com/gustavorc/arduinobluetooth/MainActivity.java
@@ -3,8 +3,11 @@ package com.gustavorc.arduinobluetooth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +16,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {

453173459098fb1abd643a95abbf2ebb670e00dc
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-26 19:18:40 -0500

    metodos para bluetooth

--- app/src/main/java/com/gustavorc/arduinobluetooth/DispositivosVinculados.java
+++ app/src/main/java/com/gustavorc/arduinobluetooth/DispositivosVinculados.java
@@ -3,9 +3,16 @@ package com.gustavorc.arduinobluetooth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Set;
 

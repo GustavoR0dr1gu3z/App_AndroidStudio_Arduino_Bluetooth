1cc105a09d80326b63b53afd658dfa556be8326a
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-27 20:00:36 -0500

    declarando variables del TextView

--- app/src/main/java/com/gustavorc/arduinobluetooth/MainActivity.java
+++ app/src/main/java/com/gustavorc/arduinobluetooth/MainActivity.java
@@ -77,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
         btnSec1 = findViewById(R.id.btnSec1);
         btnSec2 = findViewById(R.id.btnSec2);
         btnSec3 = findViewById(R.id.btnSec3);
-        txtInst = findViewById(R.id.idtxtInst);
+        txtInst = findViewById(R.id.txtInst);
 
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
